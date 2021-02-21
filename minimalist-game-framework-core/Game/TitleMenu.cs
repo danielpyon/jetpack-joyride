dcd0379bacec747aa03ee5a6432427da58753327
PYON, DANIEL <s-DPYON@lwsd.org>
2021-02-20 21:10:30 -0800

    Now displays random character

--- minimalist-game-framework-core/Game/TitleMenu.cs
+++ minimalist-game-framework-core/Game/TitleMenu.cs
@@ -6,15 +6,22 @@ class TitleMenu : Renderable
 {
     private Font font;
     private Vector2 size = new Vector2(1000, 500);
-    private Vector2 posTWO = new Vector2(30, 0);
+    private Vector2 posTWO = new Vector2(0, 0);
     private Texture background;
     private Texture backgroundTWO;
+    private Texture character;
 
     public TitleMenu()
     {
         font = Engine.LoadFont("titlefont.ttf", pointSize: 40);
         background = Engine.LoadTexture("background1.png");
         backgroundTWO = Engine.LoadTexture("titlescreen4.png");
+
+
+        String[] options = { "astronautRUNNER.png", "bodybuilderRUNNER.png", "bubblegumRUNNER.png", "clownRUNNER.png", "fireboyRUNNER.png", "kingRUNNER.png", "lizardRUNNER.png", "snakeRUNNER.png", "submarinerRUNNER.png" };
+        Random r = new Random();
+        String name = options[r.Next(0, options.Length)];
+        character = Engine.LoadTexture("clothings/" + name);
     }
 
     public void HandleInput()
@@ -34,6 +41,7 @@ class TitleMenu : Renderable
         Engine.DrawTexture(background, posTWO);
         Engine.DrawTexture(backgroundTWO, Vector2.Zero, null, size);
 
+        Engine.DrawTexture(character, new Vector2(Globals.WIDTH / 2, Globals.HEIGHT / 2));
         //Engine.DrawString("LAGS Advanced Game Studio presents", new Vector2(45, 20), Color.White, font);
         //Engine.DrawString(Globals.TITLE, new Vector2(300, 80), Color.Yellow, font);
         
